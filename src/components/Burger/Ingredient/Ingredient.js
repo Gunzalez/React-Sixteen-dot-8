570926320c6f4943da7eca2583ed06eee917ff91
Segun <gunzalez@hotmail.com>
2019-08-14 01:02:21 +0100

    Renames jsx to js

---
+++ src/components/Burger/Ingredient/Ingredient.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import classes from './Ingredient.module.scss';
+import PropTypes from 'prop-types';
+
+class Ingredient extends Component  {
+
+    render(){
+        let ingredient = null;
+        switch(this.props.type){
+            case 'bread-bottom':
+                ingredient = <div className={classes.breadBottom}></div>;
+                break;
+            case 'bread-top':
+                ingredient = (
+                    <div className={classes.breadTop}>
+                        <div className={classes.seeds}></div>
+                        <div className={classes.seeds2}></div>
+                    </div>
+                );
+                break;
+            case 'salad':
+                ingredient = <div className={classes.salad}></div>;
+                break;        
+            case 'cheese':
+                ingredient = <div className={classes.cheese}></div>;
+                break;
+            case 'meat':
+                ingredient = <div className={classes.meat}></div>;
+                break;
+            case 'bacon':
+                ingredient = <div className={classes.bacon}></div>;
+                break;
+            default:
+                ingredient = null;
+        }
+        return ingredient;        
+    }
+}
+
+Ingredient.propTypes = {
+    type: PropTypes.string.isRequired
+}
+ 
+export default Ingredient;
