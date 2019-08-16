f07428d953a3ff9f31599838b9bc47365423598e
Segun <gunzalez@hotmail.com>
2019-08-16 18:36:28 +0100

    Adds Controls component

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './BuildControls.module.scss';
+
+const buildControls = () => {
+    return ( 
+        <div className={classes.BuildControls}>
+
+        </div>
+     );
+}
+ 
+export default buildControls;
