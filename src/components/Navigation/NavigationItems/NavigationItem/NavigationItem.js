fba1380a38c2ad527c51efc35cb7b90036ffedd7
Segun <gunzalez@hotmail.com>
2019-08-18 21:34:52 +0100

    Fixes broken styling

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -5,7 +5,7 @@ import classes from './NavigationItem.module.scss';
 const NavigationItem = (props) => (
     <li className={classes.NavigationItem}>
         <a href={props.link}
-            className={props.active ? 'active' : null }>{props.children}</a>
+            className={props.active ? classes.active : null }>{props.children}</a>
     </li>
 )
  
