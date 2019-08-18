373f8e10249f8414ede8697550ac923f5c83a5d4
Segun <gunzalez@hotmail.com>
2019-08-18 23:43:55 +0100

    Close SideDrawer on clicking backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.scss';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SiderDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => {
-    return (
-        <Aux>
-            <Toolbar />
-            <SiderDrawer />
-            <main className={classes.Content}>
-                { props.children }
-            </main>
-        </Aux>
-    )
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: true
+    }
+
+    closeSideDrawerHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    render(){
+
+        const { showSideDrawer } = this.state;
+
+        return (
+            <Aux>
+                <Toolbar />
+                <SiderDrawer
+                    show={showSideDrawer}
+                    closeDrawer={this.closeSideDrawerHandler} />
+                <main className={classes.Content}>
+                    { this.props.children }
+                </main>
+            </Aux>
+        )
+    }
 }
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 import classes from './SideDrawer.module.scss';
 
 const sideDrawer = (props) => {
 
+    const attachedClasses = [classes.SideDrawer]
+    if(props.show){
+        attachedClasses.push(classes.Open);
+    } else {
+        attachedClasses.push(classes.Close);
+    }
 
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Aux>
+            <Backdrop show={props.show} cancelModal={props.closeDrawer} />
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 }
  
