9fb9dbd0d4702c5f4b522efe524a91bc4492169d
Segun <gunzalez@hotmail.com>
2019-08-29 20:04:09 +0100

    hooks up routing to elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from './hoc/Layout/Layout';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import styles from './burgerbuilder.module.css';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -8,10 +9,15 @@ import Checkout from './containers/Checkout/Checkout';
 function App() {
   return (
     <div>
-      <Layout>
-        <BurgerBuilder />
-        <Checkout />
-      </Layout> 
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route path={'/checkout'} component={Checkout} />
+            <Route path={'/builder'} component={BurgerBuilder} />
+            <Redirect to='/builder' />
+          </Switch>
+        </Layout> 
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 
 import classes from './NavigationItem.module.scss';
 
-const NavigationItem = (props) => (
+const NavigationItem = ({ link, children }) => (
     <li className={classes.NavigationItem}>
-        <a href={props.link}
-            className={props.active ? classes.active : null }>{props.children}</a>
+        <NavLink to={link} activeClassName={classes.active}>{children}</NavLink>
     </li>
 )
  
