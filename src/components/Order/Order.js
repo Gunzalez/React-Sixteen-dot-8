34f54fcb3c5fce2235d4a3a26eb4853beb1eb6cd
Segun <gunzalez@hotmail.com>
2019-09-28 21:07:45 +0100

    creates order component

---
+++ src/components/Order/Order.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Order = () => {
+    return (
+        <div>
+            <p>Ingredients Lettuce (1)</p>
+            <p>Price <strong>£5.00</strong></p>
+        </div>
+    );
+}
+ 
+
+export default Order;
